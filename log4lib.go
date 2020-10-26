09029f60185bd581cc1ae7d26360ce07457388f0
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2020-10-27 00:17:15 +0100

    Add Debug a Debugf methods

--- log4lib.go
+++ log4lib.go
@@ -2,12 +2,14 @@ package log4lib
 
 // A minimal set of operation that allow for library to use the application logger. LibLogger are created using wrappers around real logger implementations.
 type LibLogger interface {
+	Debug(args ...interface{})
 	Info(args ...interface{})
 	Warn(args ...interface{})
 	Error(args ...interface{})
 	Panic(args ...interface{})
 	Fatal(args ...interface{})
 
+	Debugf(template string, args ...interface{})
 	Infof(template string, args ...interface{})
 	Warnf(template string, args ...interface{})
 	Errorf(template string, args ...interface{})
