6e90f9f66aa364b673a3cf2144afb1bdbb2648ee
Anthony Raymond <anthonyraymond@users.noreply.github.com>
2020-10-26 14:28:31 +0100

    Update builtin-log-wrapper.go

--- log4libwrapper/builtin-log-wrapper.go
+++ log4libwrapper/builtin-log-wrapper.go
@@ -16,43 +16,43 @@ type builtinLoggerWrapper struct {
 }
 
 func (w *builtinLoggerWrapper) Info(args ...interface{}) {
-	w.delegateInfo.Println(args)
+	w.delegateInfo.Println(args...)
 }
 
 func (w *builtinLoggerWrapper) Warn(args ...interface{}) {
-	w.delegateWarn.Println(args)
+	w.delegateWarn.Println(args...)
 }
 
 func (w *builtinLoggerWrapper) Error(args ...interface{}) {
-	w.delegateError.Println(args)
+	w.delegateError.Println(args...)
 }
 
 func (w *builtinLoggerWrapper) Panic(args ...interface{}) {
-	w.delegatePanic.Panic(args)
+	w.delegatePanic.Panic(args...)
 }
 
 func (w *builtinLoggerWrapper) Fatal(args ...interface{}) {
-	w.delegateFatal.Fatal(args)
+	w.delegateFatal.Fatal(args...)
 }
 
 func (w *builtinLoggerWrapper) Infof(template string, args ...interface{}) {
-	w.delegateInfo.Println(fmt.Sprintf(template, args))
+	w.delegateInfo.Println(fmt.Sprintf(template, args...))
 }
 
 func (w *builtinLoggerWrapper) Warnf(template string, args ...interface{}) {
-	w.delegateWarn.Println(fmt.Sprintf(template, args))
+	w.delegateWarn.Println(fmt.Sprintf(template, args...))
 }
 
 func (w *builtinLoggerWrapper) Errorf(template string, args ...interface{}) {
-	w.delegateError.Println(fmt.Sprintf(template, args))
+	w.delegateError.Println(fmt.Sprintf(template, args...))
 }
 
 func (w *builtinLoggerWrapper) Panicf(template string, args ...interface{}) {
-	w.delegatePanic.Println(fmt.Sprintf(template, args))
+	w.delegatePanic.Println(fmt.Sprintf(template, args...))
 }
 
 func (w *builtinLoggerWrapper) Fatalf(template string, args ...interface{}) {
-	w.delegateFatal.Println(fmt.Sprintf(template, args))
+	w.delegateFatal.Println(fmt.Sprintf(template, args...))
 }
 
 func NewBuiltInLoggerWrapper(infoLogger *log.Logger, warnLogger *log.Logger, errorLogger *log.Logger, panicLogger *log.Logger, fatalLogger *log.Logger) log4lib.LibLogger {
